32edc0dc79ddd9dc9fff963ede75a3e27fd9bf0f
CurtoBrull <curto.brull.javier@gmail.com>
2024-07-10 11:33:06 +0200

    JCB web ASTRO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://jcurtobr.eu/',
+});
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Javier Curto';
+export const SITE_DESCRIPTION = 'Desarrollo de aplicaciones y páginas web';
+export const ICON = '/src/assets/img/jcb.png';
